e839ad24f06bd1b140098041cabfa37b35f98f8b
narmijo-tj <toffee.jaguargmail.com>
2024-10-13 06:42:10 -0300

    Al parecer las dependencias del recycleradapter de firestore se pudo resolver y compilar la app. Solo hubo que importar todas las dependencias. LAs otras funcionalidades de login, registro y agregar nota funcionan en la base de datos, pero aun no se prueba si se pueden listar

--- app/src/main/java/com/narmijo/notasfirebase/ListarNotas/Listar_Notas.java
+++ app/src/main/java/com/narmijo/notasfirebase/ListarNotas/Listar_Notas.java
@@ -1,14 +1,7 @@
 package com.narmijo.notasfirebase.ListarNotas;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -17,37 +10,34 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.narmijo.notasfirebase.ActualizarNota.Actualizar_Nota;
 import com.narmijo.notasfirebase.Detalle.Detalle_Nota;
 import com.narmijo.notasfirebase.Objetos.Nota;
 import com.narmijo.notasfirebase.R;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.narmijo.notasfirebase.ViewHolder.ViewHolder_Nota;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
 
 import org.jetbrains.annotations.NotNull;
 
 public class Listar_Notas extends AppCompatActivity {
 
     RecyclerView recyclerviewNotas;
-    FirebaseDatabase firebaseDatabase;
-    DatabaseReference BASE_DE_DATOS;
-
-    LinearLayoutManager linearLayoutManager;
-
-    FirebaseRecyclerAdapter<Nota, ViewHolder_Nota> firebaseRecyclerAdapter;
-    FirebaseRecyclerOptions<Nota> options;
+    FirebaseFirestore firestore;
+    FirestoreRecyclerAdapter<Nota, ViewHolder_Nota> firestoreRecyclerAdapter;
+    FirestoreRecyclerOptions<Nota> options;
 
     Dialog dialog;
-
     FirebaseAuth auth;
     FirebaseUser user;
 
@@ -67,19 +57,21 @@ public class Listar_Notas extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
 
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        BASE_DE_DATOS = firebaseDatabase.getReference("Notas_Publicadas");
+        firestore = FirebaseFirestore.getInstance();
         dialog = new Dialog(Listar_Notas.this);
-        dialog = new Dialog(Listar_Notas.this);
-        ListarNotasUsuarios();
 
+        ListarNotasUsuarios();
     }
 
     private void ListarNotasUsuarios(){
-        //consulta
-        Query query = BASE_DE_DATOS.orderByChild("uid_usuario").equalTo(user.getUid());
-        options = new FirebaseRecyclerOptions.Builder<Nota>().setQuery(query, Nota.class).build();
-        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Nota, ViewHolder_Nota>(options) {
+        Query query = firestore.collection("Notas_Publicadas")
+                .whereEqualTo("uid_usuario", user.getUid());
+
+        options = new FirestoreRecyclerOptions.Builder<Nota>()
+                .setQuery(query, Nota.class)
+                .build();
+
+        firestoreRecyclerAdapter = new FirestoreRecyclerAdapter<Nota, ViewHolder_Nota>(options) {
             @Override
             protected void onBindViewHolder(@NonNull ViewHolder_Nota viewHolder_nota, int position, @NotNull Nota nota) {
                 viewHolder_nota.SetearDatos(
@@ -95,137 +87,85 @@ public class Listar_Notas extends AppCompatActivity {
                 );
             }
 
-
             @Override
-            public ViewHolder_Nota onCreateViewHolder(@NonNull  ViewGroup parent, int viewType) {
-                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_nota,parent,false);
+            public ViewHolder_Nota onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_nota, parent, false);
                 ViewHolder_Nota viewHolder_nota = new ViewHolder_Nota(view);
                 viewHolder_nota.setOnClickListener(new ViewHolder_Nota.ClickListener() {
                     @Override
                     public void onItemClick(View view, int position) {
-
-                        //Obtener los datos de la nota seleccionada
-                        String id_nota = getItem(position).getId_nota();
-                        String uid_usuario = getItem(position).getUid_usuario();
-                        String correo_usuario = getItem(position).getCorreo_usuario();
-                        String fecha_registro = getItem(position).getFecha_hora_actual();
-                        String titulo = getItem(position).getTitulo();
-                        String descripcion = getItem(position).getDescripcion();
-                        String fecha_nota = getItem(position).getFecha_nota();
-                        String estado = getItem(position).getEstado();
-
-                        //Enviamos los datos a la siguiente actividad
+                        // Obtenemos los datos de la nota seleccionada
+                        Nota selectedNota = getItem(position);
                         Intent intent = new Intent(Listar_Notas.this, Detalle_Nota.class);
-                        intent.putExtra("id_nota", id_nota);
-                        intent.putExtra("uid_usuario", uid_usuario);
-                        intent.putExtra("correo_usuario", correo_usuario);
-                        intent.putExtra("fecha_registro", fecha_registro);
-                        intent.putExtra("titulo", titulo);
-                        intent.putExtra("descripcion", descripcion);
-                        intent.putExtra("fecha_nota", fecha_nota);
-                        intent.putExtra("estado", estado);
+                        intent.putExtra("id_nota", selectedNota.getId_nota());
+                        intent.putExtra("uid_usuario", selectedNota.getUid_usuario());
+                        intent.putExtra("correo_usuario", selectedNota.getCorreo_usuario());
+                        intent.putExtra("fecha_registro", selectedNota.getFecha_hora_actual());
+                        intent.putExtra("titulo", selectedNota.getTitulo());
+                        intent.putExtra("descripcion", selectedNota.getDescripcion());
+                        intent.putExtra("fecha_nota", selectedNota.getFecha_nota());
+                        intent.putExtra("estado", selectedNota.getEstado());
                         startActivity(intent);
                     }
 
                     @Override
                     public void onItemLongClick(View view, int position) {
-
-                        //Obtener los datos de la nota seleccionada
-                        String id_nota = getItem(position).getId_nota();
-                        String uid_usuario = getItem(position).getUid_usuario();
-                        String correo_usuario = getItem(position).getCorreo_usuario();
-                        String fecha_registro = getItem(position).getFecha_hora_actual();
-                        String titulo = getItem(position).getTitulo();
-                        String descripcion = getItem(position).getDescripcion();
-                        String fecha_nota = getItem(position).getFecha_nota();
-                        String estado = getItem(position).getEstado();
-
-                        //Declarar las vistas
-                        Button CD_Eliminar, CD_Actualizar;
-
-                        //Realizar la conexión con el diseño
-                        dialog.setContentView(R.layout.dialogo_opciones);
-
-                        //Inicializar las vistas
-                        CD_Eliminar = dialog.findViewById(R.id.CD_Eliminar);
-                        CD_Actualizar = dialog.findViewById(R.id.CD_Actualizar);
-
-                        CD_Eliminar.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                EliminarNota(id_nota);
-                                dialog.dismiss();
-                            }
-                        });
-
-                        CD_Actualizar.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                //Toast.makeText(Listar_Notas.this, "Actualizar nota", Toast.LENGTH_SHORT).show();
-                                //startActivity(new Intent(Listar_Notas.this, Actualizar_Nota.class));
-                                Intent intent = new Intent(Listar_Notas.this, Actualizar_Nota.class);
-                                intent.putExtra("id_nota", id_nota);
-                                intent.putExtra("uid_usuario", uid_usuario);
-                                intent.putExtra("correo_usuario", correo_usuario);
-                                intent.putExtra("fecha_registro", fecha_registro);
-                                intent.putExtra("titulo", titulo);
-                                intent.putExtra("descripcion", descripcion);
-                                intent.putExtra("fecha_nota", fecha_nota);
-                                intent.putExtra("estado", estado);
-                                startActivity(intent);
-                                dialog.dismiss();
-
-                            }
-                        });
-                        dialog.show();
+                        // Obtener datos para eliminar/actualizar
+                        Nota selectedNota = getItem(position);
+                        mostrarOpciones(selectedNota);
                     }
                 });
                 return viewHolder_nota;
             }
         };
 
-        linearLayoutManager = new LinearLayoutManager(Listar_Notas.this, LinearLayoutManager.VERTICAL, false);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(Listar_Notas.this, LinearLayoutManager.VERTICAL, false);
         linearLayoutManager.setReverseLayout(true);
         linearLayoutManager.setStackFromEnd(true);
 
         recyclerviewNotas.setLayoutManager(linearLayoutManager);
-        recyclerviewNotas.setAdapter(firebaseRecyclerAdapter);
+        recyclerviewNotas.setAdapter(firestoreRecyclerAdapter);
+    }
 
+    private void mostrarOpciones(Nota nota) {
+        dialog.setContentView(R.layout.dialogo_opciones);
+        Button CD_Eliminar = dialog.findViewById(R.id.CD_Eliminar);
+        Button CD_Actualizar = dialog.findViewById(R.id.CD_Actualizar);
+
+        CD_Eliminar.setOnClickListener(view -> {
+            EliminarNota(nota.getId_nota());
+            dialog.dismiss();
+        });
+
+        CD_Actualizar.setOnClickListener(view -> {
+            Intent intent = new Intent(Listar_Notas.this, Actualizar_Nota.class);
+            intent.putExtra("id_nota", nota.getId_nota());
+            intent.putExtra("uid_usuario", nota.getUid_usuario());
+            intent.putExtra("correo_usuario", nota.getCorreo_usuario());
+            intent.putExtra("fecha_registro", nota.getFecha_hora_actual());
+            intent.putExtra("titulo", nota.getTitulo());
+            intent.putExtra("descripcion", nota.getDescripcion());
+            intent.putExtra("fecha_nota", nota.getFecha_nota());
+            intent.putExtra("estado", nota.getEstado());
+            startActivity(intent);
+            dialog.dismiss();
+        });
+
+        dialog.show();
     }
 
     private void EliminarNota(String id_nota) {
-
         AlertDialog.Builder builder = new AlertDialog.Builder(Listar_Notas.this);
         builder.setTitle("Eliminar nota");
         builder.setMessage("¿Desea eliminar la nota?");
-        builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                //ELIMINAR NOTA EN BD
-                Query query = BASE_DE_DATOS.orderByChild("id_nota").equalTo(id_nota);
-                query.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                        for (DataSnapshot ds : snapshot.getChildren()){
-                            ds.getRef().removeValue();
-                        }
-                        Toast.makeText(Listar_Notas.this, "Nota eliminada", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull @NotNull DatabaseError error) {
-                        Toast.makeText(Listar_Notas.this, error.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
+        builder.setPositiveButton("Sí", (dialogInterface, i) -> {
+            firestore.collection("Notas_Publicadas").document(id_nota)
+                    .delete()
+                    .addOnSuccessListener(aVoid -> Toast.makeText(Listar_Notas.this, "Nota eliminada", Toast.LENGTH_SHORT).show())
+                    .addOnFailureListener(e -> Toast.makeText(Listar_Notas.this, e.getMessage(), Toast.LENGTH_SHORT).show());
         });
 
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(Listar_Notas.this, "Cancelado por el usuario", Toast.LENGTH_SHORT).show();
-            }
-        });
+        builder.setNegativeButton("No", (dialogInterface, i) -> Toast.makeText(Listar_Notas.this, "Cancelado por el usuario", Toast.LENGTH_SHORT).show());
 
         builder.create().show();
     }
@@ -233,8 +173,8 @@ public class Listar_Notas extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        if (firebaseRecyclerAdapter!=null){
-            firebaseRecyclerAdapter.startListening();
+        if (firestoreRecyclerAdapter != null) {
+            firestoreRecyclerAdapter.startListening();
         }
     }
 
